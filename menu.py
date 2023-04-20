4dfad5806c64ecbcbaf9e84064fa44d66845f23f
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-20 22:14:01 +0300

    menu.py handle exceptions

--- menu.py
+++ menu.py
@@ -20,20 +20,24 @@ def main_menu():
     return choice
 
 def handle_choice(choice):
-    if choice == 1:
-        create_account()
-    elif choice == 2:
-        view_balance()
-    elif choice == 3:
-        send_transaction()
-    elif choice == 4:
-        load_account()
     if choice == 0:
         quit()
-    else:
-        print("Неправильний номер меню.")
-    input("Натисніть Enter для продовження...")
-    clear_screen()
+    try:
+        if choice == 1:
+            create_account()
+        elif choice == 2:
+            view_balance()
+        elif choice == 3:
+            send_transaction()
+        elif choice == 4:
+            load_account()
+        else:
+            print("Неправильний номер меню.")
+    except:
+        pass
+    finally:
+        input("Натисніть Enter для продовження...")
+        clear_screen()
 
 if __name__ == '__main__':
     menu()
