daa3dedf9627bc1cd44cca993e4717143249dbd8
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-20 18:40:05 +0300

    main.py add commands

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ import db
 def main():
 	db.create_table()
 
+# додання команди з cli.py
+main.add_command(cli.create_account)
+main.add_command(cli.load_account)
 if __name__ == '__main__':
     main()
