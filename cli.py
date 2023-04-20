0f43433aa5c4b4a5c50bef4300f972cbbaa57dc1
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-20 18:34:26 +0300

    eth and cli create account

--- cli.py
+++ cli.py
@@ -5,3 +5,10 @@ import auth
 
 cli = click.Group()
 
+@click.command()
+@click.option("--username", prompt=True, help="Ім'я користувача для нового облікового запису")
+@click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True, help="Пароль для нового облікового запису")
+def create_account(username, password):
+    account = eth.create_account()
+    db.add_user(username, password, account["address"], account["private_key"])
+
--- eth.py
+++ eth.py
@@ -4,3 +4,8 @@ import os
 
 # Підключенні до мережі
 web3 = Web3(Web3.HTTPProvider('http://127.0.0.1:7545'))
+
+def create_account():
+    account = Account.create()
+    return {"address": account.address, "private_key": account.key.hex()}
+
