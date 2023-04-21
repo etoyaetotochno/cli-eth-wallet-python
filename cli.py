136fa50180a9610ea6484928134aa5bad751fb49
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-21 17:01:42 +0300

    cli.py allow to choose the sender address

--- cli.py
+++ cli.py
@@ -37,15 +37,27 @@ def load_account(username, password, key):
 @click.command()
 @click.option("--username", prompt=True, help="Ім'я користувача існуючого облікового запису")
 @click.option("--password", prompt=True, hide_input=True, help="Пароль існуючого облікового запису")
+@click.option("--sender_address", prompt=False, default=None, help="Адреса відправника")
 @click.option("--to_address", prompt=True, help="Адреса призначення")
 @click.option("--value", prompt=True, type=float, help="Сума (ефіру) транзакції")
-def send_transaction(username, password, to_address, value):
+def send_transaction(username, password, sender_address, to_address, value):
     user = auth.authenticate(username, password)
     if not user:
         click.echo("Неправильне ім'я або пароль")
         return
-    private_key = db.get_private_key(user["address"])[0][0]
-    tx_hash = eth.send_transaction(private_key, to_address, value)
+    if sender_address:
+        private_key = db.get_private_key(sender_address)[0][0]
+        tx_hash = eth.send_transaction(private_key, to_address, value)
+    else:
+        addresses = [row[0] for row in db.user_addresses(username)]
+        user_choice = click.prompt(
+            "Введіть вихідний рахунок:",
+            type=click.Choice(addresses),
+            show_choices=True,
+            prompt_suffix="\n"
+        )
+        private_key = db.get_private_key(user_choice)[0][0]
+        tx_hash = eth.send_transaction(private_key, to_address, value)
     click.echo("Транзакцію відправлено: {}".format(tx_hash))
 
 @click.command()
