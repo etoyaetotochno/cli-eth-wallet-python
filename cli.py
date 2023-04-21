7b1fc7396a788b439fb295c7b1e202a0df15c759
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-21 10:29:19 +0300

    cli.py change allow load to existing account

--- cli.py
+++ cli.py
@@ -21,9 +21,18 @@ def create_account(username, password):
 @click.option("--password", prompt=True, hide_input=True, confirmation_prompt=True, help="Пароль для нового облікового запису")
 @click.option("--key", prompt=True, help="Приватний ключ існуючого рахунку в мережі")
 def load_account(username, password, key):
-    account = eth.load_account(key)
-    db.add_user(username, password, account["address"], account["private_key"])
-    click.echo("Рахунок імпортовано:\nІм'я користувача: {}\nАдреса рахунку: {}".format(username, account["address"]))
+    if db.user_unique(username):
+        db.add_user(username, password, account["address"], account["private_key"])
+        click.echo("Рахунок імпортовано до нового облікового запису:\nІм'я користувача: {}\nАдреса рахунку: {}".format(username, account["address"]))
+    else:
+        user = auth.authenticate(username, password)
+        if not user:
+            click.echo("Неправильне ім'я або пароль")
+            return
+        else:
+            account = eth.load_account(key)
+            db.add_user(username, password, account["address"], account["private_key"])
+            click.echo("Рахунок імпортовано до існуючого облікового запису:\nІм'я користувача: {}\nАдреса рахунку: {}".format(username, account["address"]))
 
 @click.command()
 @click.option("--username", prompt=True, help="Ім'я користувача існуючого облікового запису")
