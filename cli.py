f91ccad92083df9bbe2837a2bcfcc99e231be58e
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-21 16:48:48 +0300

    cli.py rename

--- cli.py
+++ cli.py
@@ -39,13 +39,13 @@ def load_account(username, password, key):
 @click.option("--password", prompt=True, hide_input=True, help="Пароль існуючого облікового запису")
 @click.option("--to_address", prompt=True, help="Адреса призначення")
 @click.option("--value", prompt=True, type=float, help="Сума (ефіру) транзакції")
-def send_transaction(username, password, to, value):
+def send_transaction(username, password, to_address, value):
     user = auth.authenticate(username, password)
     if not user:
         click.echo("Неправильне ім'я або пароль")
         return
     private_key = db.get_private_key(user["address"])[0][0]
-    tx_hash = eth.send_transaction(private_key, to, value)
+    tx_hash = eth.send_transaction(private_key, to_address, value)
     click.echo("Транзакцію відправлено: {}".format(tx_hash))
 
 @click.command()
