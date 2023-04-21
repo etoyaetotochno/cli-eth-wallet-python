acb141be037964c99837ffe77796dfaa5a7d44e9
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-21 18:37:35 +0300

    cli.py handle duplicate accounts

--- cli.py
+++ cli.py
@@ -36,8 +36,11 @@ def load_account(username, password, key):
             return
         else:
             account = eth.load_account(key)
-            db.add_user(username, password, account["address"], account["private_key"])
-            click.echo("Рахунок імпортовано до існуючого облікового запису:\nІм'я користувача: {}\nАдреса рахунку: {}".format(username, account["address"]))
+            if db.address_unique(account["address"]):
+                db.add_user(username, password, account["address"], account["private_key"])
+                click.echo("Рахунок імпортовано до існуючого облікового запису:\nІм'я користувача: {}\nАдреса рахунку: {}".format(username, account["address"]))
+            else:
+                click.echo("Помилка! Рахунок вже існує.")
 
 @click.command()
 @click.option("--username", prompt=True, help="Ім'я користувача існуючого облікового запису")
