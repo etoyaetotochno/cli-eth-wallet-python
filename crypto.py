ef1164cdd8da402a94c2a900612bf5a54b501d44
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-22 15:16:08 +0300

    crypto random string

--- crypto.py
+++ crypto.py
@@ -2,3 +2,8 @@ import secrets
 from cryptography.fernet import Fernet
 from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 from cryptography.hazmat.primitives import hashes
+
+def random_string(length):
+    random_string = secrets.token_bytes(length)
+    return random_string
+
