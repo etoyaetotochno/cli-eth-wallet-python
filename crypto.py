b257087900143ead385b0ddf693c23b75eb04ab3
etoyaetotochno <55183420+etoyaetotochno@users.noreply.github.com>
2023-04-22 15:48:09 +0300

    crypto.py encrypt and decrypt

--- crypto.py
+++ crypto.py
@@ -22,3 +22,19 @@ def generate_key_from_password(password, salt=None):
     key = kdf.derive(password.encode())
     return base64.urlsafe_b64encode(key), salt.hex()
 
+def encrypt_private_key(private_key_hex, password):
+    private_key_hex = private_key_hex[2:]
+    key, salt = generate_key_from_password(password)
+    f = Fernet(key)
+    private_key_bytes = bytes.fromhex(private_key_hex)
+    encrypted_private_key = f.encrypt(private_key_bytes)
+    return encrypted_private_key.decode() + "." + salt
+
+def decrypt_private_key(encrypted, password):
+    encrypted_private_key, salt = encrypted.split(".")
+    salt = bytes.fromhex(salt)
+    key, salt = generate_key_from_password(password, salt)
+    f = Fernet(key)
+    private_key_bytes = f.decrypt(encrypted_private_key)
+    return "0x" + private_key_bytes.hex()
+
